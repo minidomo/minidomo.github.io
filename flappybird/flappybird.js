a6ac0c1a2499fb8ff4ecc0afd8cd9c701b8e56a2
MiniDomo <32505118+MiniDomo@users.noreply.github.com>
2018-04-15 20:02:41 -0500

    Added Code.org Projects

---
+++ flappybird/flappybird.js
@@ -0,0 +1,78 @@
+document.onmousedown = mouseDown;
+document.onmouseup = mouseUp;
+document.ontouchstart = touchStart;
+document.ontouchend = touchEnd;
+
+var player = {
+    w: 25,
+    h: 25,
+    x: 220,
+    y: 269,
+    velY: 0,
+    maxVelY: 8,
+    isJumping: false
+};
+
+var running = false;
+var gravity = 0.6;
+
+function update() {
+    if (!running)
+        clearInterval(update());
+    playerUpdate();
+}
+
+function reset() {
+    player.x = 220;
+    player.y = 269;
+    player.velY = 0;
+    document.getElementById('bird').style.left = player.x + 'px';
+    document.getElementById('bird').style.top = player.y + 'px';
+}
+
+function playerUpdate() {
+    fall();
+    player.y += player.velY;
+    if (player.isJumping)
+        player.velY = -8;
+    player.y = clamp(player.y, 0, 588 - player.w);
+    document.getElementById('bird').style.top = player.y + 'px';
+}
+
+function fall() {
+    player.velY += gravity;
+    if (player.velY >= player.maxVelY)
+        player.velY = player.maxVelY;
+}
+
+function clamp(val, min, max) {
+    if (val >= max)
+        return max;
+    else if (val <= min)
+        return min;
+    else
+        return val;
+}
+
+function mouseDown(ev) {
+    player.isJumping = true;
+}
+
+function mouseUp(ev) {
+    player.isJumping = false;
+}
+
+function touchStart(e) {
+    player.isJumping = true;
+}
+
+function touchEnd(e) {
+    player.isJumping = false;
+}
+
+function start() {
+    // alert("working");
+    running = true;
+    reset();
+    setInterval("update()", 17);
+}
