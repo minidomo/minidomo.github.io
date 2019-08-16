8378978bc48ec1c142313b1e01709bc135d40b82
JB Ladera <32505118+MiniDomo@users.noreply.github.com>
2019-08-15 23:00:55 -0500

    switched to jquery

--- AnniCalc/main.js
+++ AnniCalc/main.js
@@ -87,7 +87,6 @@ const TDTH = {
     add(id, val) {
         const tableRowFirst = document.getElementById('ingredients-first-row'),
             tableRowSecond = document.getElementById('ingredients-second-row');
-
         const th = document.createElement('th');
         th.id = id;
         const img = new Image();
@@ -130,51 +129,65 @@ let includeStand = false;
 let excludeRedstoneCost = false;
 
 const update = element => {
-    const tableRowChildren = element.parentElement.parentElement.childNodes;
-
-    let sets = tableRowChildren[3].childNodes[1].value,
-        brewingStands = tableRowChildren[5].childNodes[1].value,
-        potionName = tableRowChildren[1].childNodes[1].value,
+    const tableRowChildren = element.parent().parent().children();
+    const [dropdownTd, setsTd, brewingStandTd, totalGold] = tableRowChildren.map(index => $(tableRowChildren[index]));
+    const Sets = {
+        td: setsTd,
+        input: setsTd.children().first()
+    };
+    const BrewingStands = {
+        td: brewingStandTd,
+        input: brewingStandTd.children().first()
+    };
+    const PotionOption = {
+        td: dropdownTd,
+        selection: dropdownTd.children().first()
+    };
+    let sets = Sets.input.val(),
+        brewingStands = BrewingStands.input.val(),
+        potionName = PotionOption.selection.val(),
         extra = 0;
+
+
 
     if (isNum(sets)) {
         sets = parseInt(sets);
-        tableRowChildren[3].style.backgroundColor = 'white';
-        tableRowChildren[3].childNodes[1].style.backgroundColor = 'white';
+        Sets.td.css('backgroundColor', 'white');
+        Sets.input.css('backgroundColor', 'white');
     } else {
-        if (sets !== '') {
-            tableRowChildren[3].style.backgroundColor = '#FF495C';
-            tableRowChildren[3].childNodes[1].style.backgroundColor = '#FF495C';
+        if (sets) {
+            Sets.td.css('backgroundColor', '#FF495C');
+            Sets.input.css('backgroundColor', '#FF495C');
         } else {
-            tableRowChildren[3].style.backgroundColor = 'white';
-            tableRowChildren[3].childNodes[1].style.backgroundColor = 'white';
+            Sets.td.css('backgroundColor', 'white');
+            Sets.input.css('backgroundColor', 'white');
         }
         sets = 0;
     }
 
     if (isNum(brewingStands)) {
         brewingStands = parseInt(brewingStands);
-        tableRowChildren[5].style.backgroundColor = 'white';
-        tableRowChildren[5].childNodes[1].style.backgroundColor = 'white';
+        BrewingStands.td.css('backgroundColor', 'white');
+        BrewingStands.input.css('backgroundColor', 'white');
     } else {
-        if (brewingStands !== '') {
-            tableRowChildren[5].style.backgroundColor = '#FF495C';
-            tableRowChildren[5].childNodes[1].style.backgroundColor = '#FF495C';
+        if (brewingStands) {
+            BrewingStands.td.css('backgroundColor', '#FF495C');
+            BrewingStands.input.css('backgroundColor', '#FF495C');
         } else {
-            tableRowChildren[5].style.backgroundColor = 'white';
-            tableRowChildren[5].childNodes[1].style.backgroundColor = 'white';
+            BrewingStands.td.css('backgroundColor', 'white');
+            BrewingStands.input.css('backgroundColor', 'white');
         }
         brewingStands = 0;
     }
 
     if (includeStand) {
-        const cell = element.parentElement.cellIndex;
+        const cell = element.parent()[0].cellIndex;
         if (cell === 1) {
             brewingStands = sets;
-            tableRowChildren[5].childNodes[1].value = sets === 0 ? '' : sets;
+            BrewingStands.input.val(brewingStands === 0 ? '' : brewingStands);
         } else if (cell == 2) {
             sets = brewingStands;
-            tableRowChildren[3].childNodes[1].value = brewingStands === 0 ? '' : brewingStands;
+            Sets.input.val(sets === 0 ? '' : sets);
         }
     }
 
@@ -185,71 +198,84 @@ const update = element => {
             if (word === 'Ext.') {
                 if (!excludeRedstoneCost)
                     extra += BrewingShop.Redstone;
-            } else if (!(word === 'II' || word === 'Splash'))
+            } else if (word !== 'II' && word !== 'Splash')
                 potionName += word;
         });
     }
 
     const goldForRow = (sets * (Potion[potionName].Cost + extra)) + (brewingStands * BrewingShop.BrewingStand);
-
-    tableRowChildren[7].textContent = showStacks ? convertToStacks(goldForRow) : goldForRow;
+    totalGold.text(showStacks ? convertToStacks(goldForRow) : goldForRow);
     readPotionChart();
 }
 
 const readPotionChart = () => {
-    const tableChildren = document.getElementById('potionChart').childNodes[1].childNodes;
+    const table = $('#potionChart').children().first().children();
     let total = 0;
     resetTotals();
-    for (let x = 2; x <= 22; x += 2) {
-        let theTotalGold = tableChildren[x].childNodes[7].innerHTML;
-        if (theTotalGold.includes(' ')) {
-            let arr = theTotalGold.split(' ');
+    for (let x = 1; x < table.length - 1; x++) {
+        const tableRowChildren = $(table[x]).children();
+        const [dropdownTd, setsTd, brewingStandTd, totalGold] = tableRowChildren.map(index => $(tableRowChildren[index]));
+        const Sets = {
+            td: setsTd,
+            input: setsTd.children().first()
+        };
+        const BrewingStands = {
+            td: brewingStandTd,
+            input: brewingStandTd.children().first()
+        };
+        const PotionOption = {
+            td: dropdownTd,
+            selection: dropdownTd.children().first()
+        };
+
+        if (totalGold.text().includes(' ')) {
+            let arr = totalGold.text().split(' ');
             if (arr.length > 2)
                 continue;
-            theTotalGold = '' + convertToBlocks(theTotalGold);
+            totalGold.text(convertToBlocks(totalGold.text()));
         }
-        if (isNum(theTotalGold)) {
-            let setcount = tableChildren[x].childNodes[3].childNodes[1].value,
-                standcount = tableChildren[x].childNodes[5].childNodes[1].value,
-                nameofpotion = tableChildren[x].childNodes[1].childNodes[1].value;
 
-            let hasExt = nameofpotion.includes('Ext.');
+        if (isNum(totalGold.text())) {
+            let sets = Sets.input.val(),
+                brewingStands = BrewingStands.input.val(),
+                potionName = PotionOption.selection.val();
 
-            if (nameofpotion.includes(' ')) {
-                const arr = nameofpotion.split(' ');
-                nameofpotion = '';
+            let hasExt = potionName.includes('Ext.');
+
+            if (potionName.includes(' ')) {
+                const arr = potionName.split(' ');
+                potionName = '';
                 arr.forEach(word => {
-                    if (isNum(setcount)) {
+                    if (isNum(sets)) {
                         if (word === 'Ext.')
-                            Totals.redstone += parseInt(setcount);
+                            Totals.redstone += parseInt(sets);
                         else if (word === 'II')
-                            Totals.glowstone_dust += parseInt(setcount);
+                            Totals.glowstone_dust += parseInt(sets);
                         else if (word === 'Splash')
-                            Totals.gunpowder += parseInt(setcount);
+                            Totals.gunpowder += parseInt(sets);
                     }
-                    if (!(word === 'II' || word === 'Splash' || word === 'Ext.'))
-                        nameofpotion += word;
+                    if (word !== 'II' && word !== 'Splash' && word !== 'Ext.')
+                        potionName += word;
                 });
             }
 
-            if (isNum(standcount) && standcount !== '0')
-                Totals.brewing_stand += parseInt(standcount);
+            if (isNum(brewingStands) && brewingStands !== '0')
+                Totals.brewing_stand += parseInt(brewingStands);
 
-            if (isNum(setcount) && setcount !== '0') {
-                const arr = Potion[nameofpotion].Recipe;
+            if (isNum(sets)) {
+                const arr = Potion[potionName].Recipe;
                 arr.forEach(x => {
-                    Totals[x] += parseInt(setcount);
+                    Totals[x] += parseInt(sets);
                 });
             }
-
-            setcount = isNum(setcount) ? parseInt(setcount) : 0;
-            standcount = isNum(standcount) ? parseInt(standcount) : 0;
-            const current = (setcount * (Potion[nameofpotion].Cost + (!hasExt ? 0 : (excludeRedstoneCost ? 0 : 3)))) + (standcount * BrewingShop.BrewingStand);
+            sets = isNum(sets) ? parseInt(sets) : 0;
+            brewingStands = isNum(brewingStands) ? parseInt(brewingStands) : 0;
+            const current = (sets * (Potion[potionName].Cost + (!hasExt ? 0 : (excludeRedstoneCost ? 0 : 3)))) + (brewingStands * BrewingShop.BrewingStand);
             total += current;
-            tableChildren[x].childNodes[7].innerHTML = showStacks ? convertToStacks(current) : current;
+            totalGold.text(showStacks ? convertToStacks(current) : current);
         }
     }
-    tableChildren[24].childNodes[7].textContent = showStacks ? convertToStacks(total) : total;
+    table.last().children().last().text(showStacks ? convertToStacks(total) : total);
     for (const prop in IngredientsTable) {
         if (IngredientsTable[prop] !== Totals[prop]) {
             if (Totals[prop] === 0) {
@@ -282,57 +308,68 @@ const convertToBlocks = stacksAndBlocks => {
     return parseInt(arr[0]) * 64 + parseInt(arr[1]);
 }
 
-const conversionButton = document.getElementById('conversionButton');
-const standButton = document.getElementById('includeStand');
-const redstoneButton = document.getElementById('excludeRedstoneCost');
+$(document).ready(() => {
 
-conversionButton.addEventListener('click', function () {
-    showStacks = !showStacks;
-    this.textContent = showStacks ? 'Stacks' : 'Blocks';
-    for (let x = 2; x <= 24; x += 2) {
-        const totalGoldDisplay = document.getElementById('potionChart').childNodes[1].childNodes[x].childNodes[7];
-        totalGoldDisplay.textContent = showStacks ? convertToStacks(parseInt(totalGoldDisplay.textContent)) : convertToBlocks(totalGoldDisplay.textContent);
+    const conversionButton = $('#conversionButton');
+    conversionButton.click(function () {
+        showStacks = !showStacks;
+        this.textContent = showStacks ? 'Stacks' : 'Blocks';
+        const totalGoldRows = $('.totalGoldRow');
+        for (const cell of totalGoldRows) {
+            const e = $(cell);
+            const current = e.text();
+            const converted = showStacks ? convertToStacks(parseInt(current)) : convertToBlocks(current);
+            e.text(converted);
+        }
+    });
+    conversionButton.mouseover(function () {
+        this.style.backgroundColor = showStacks ? '#D8315B' : 'white';
+        this.style.color = showStacks ? '#FFFAFF' : '#D8315B';
+    });
+    conversionButton.mouseleave(function () {
+        this.style.backgroundColor = !showStacks ? '#D8315B' : 'white';
+        this.style.color = !showStacks ? '#FFFAFF' : '#D8315B';
+    });
+
+    const standButton = $('#includeStand');
+    standButton.click(function () {
+        includeStand = !includeStand;
+        this.textContent = 'With' + (includeStand ? ' ' : 'out ') + 'Stand';
+    });
+    standButton.mouseover(function () {
+        this.style.backgroundColor = includeStand ? '#D8315B' : 'white';
+        this.style.color = includeStand ? '#FFFAFF' : '#D8315B';
+    });
+    standButton.mouseleave(function () {
+        this.style.backgroundColor = !includeStand ? '#D8315B' : 'white';
+        this.style.color = !includeStand ? '#FFFAFF' : '#D8315B';
+    });
+
+    const redstoneButton = $('#excludeRedstoneCost');
+    redstoneButton.click(function () {
+        excludeRedstoneCost = !excludeRedstoneCost;
+        this.textContent = (excludeRedstoneCost ? 'Ex' : 'In') + 'clude Redstone Cost';
+        // TODO make this better
+        readPotionChart();
+    });
+    redstoneButton.mouseover(function () {
+        this.style.backgroundColor = excludeRedstoneCost ? '#D8315B' : 'white';
+        this.style.color = excludeRedstoneCost ? '#FFFAFF' : '#D8315B';
+    });
+    redstoneButton.mouseleave(function () {
+        this.style.backgroundColor = !excludeRedstoneCost ? '#D8315B' : 'white';
+        this.style.color = !excludeRedstoneCost ? '#FFFAFF' : '#D8315B';
+    });
+
+    const potionChartInputs = $('.potionChartInput');
+    for (const input of potionChartInputs) {
+        const e = $(input);
+        e.keyup(() => update(e));
     }
-});
 
-conversionButton.addEventListener('mouseover', function () {
-    this.style.backgroundColor = showStacks ? '#D8315B' : 'white';
-    this.style.color = showStacks ? '#FFFAFF' : '#D8315B';
-});
-
-conversionButton.addEventListener('mouseleave', function () {
-    this.style.backgroundColor = !showStacks ? '#D8315B' : 'white';
-    this.style.color = !showStacks ? '#FFFAFF' : '#D8315B';
-});
-
-standButton.addEventListener('click', function () {
-    includeStand = !includeStand;
-    this.textContent = 'With' + (includeStand ? ' ' : 'out ') + 'Stand';
-});
-
-standButton.addEventListener('mouseover', function () {
-    this.style.backgroundColor = includeStand ? '#D8315B' : 'white';
-    this.style.color = includeStand ? '#FFFAFF' : '#D8315B';
-});
-
-standButton.addEventListener('mouseleave', function () {
-    this.style.backgroundColor = !includeStand ? '#D8315B' : 'white';
-    this.style.color = !includeStand ? '#FFFAFF' : '#D8315B';
-});
-
-redstoneButton.addEventListener('click', function () {
-    excludeRedstoneCost = !excludeRedstoneCost;
-    this.textContent = (excludeRedstoneCost ? 'Ex' : 'In') + 'clude Redstone Cost';
-    // TODO make this better
-    readPotionChart();
-});
-
-redstoneButton.addEventListener('mouseover', function () {
-    this.style.backgroundColor = excludeRedstoneCost ? '#D8315B' : 'white';
-    this.style.color = excludeRedstoneCost ? '#FFFAFF' : '#D8315B';
-});
-
-redstoneButton.addEventListener('mouseleave', function () {
-    this.style.backgroundColor = !excludeRedstoneCost ? '#D8315B' : 'white';
-    this.style.color = !excludeRedstoneCost ? '#FFFAFF' : '#D8315B';
+    const dropdowns = $('.dropdown');
+    for (const dropdown of dropdowns) {
+        const e = $(dropdown);
+        e.mouseup(() => update(e));
+    }
 });
